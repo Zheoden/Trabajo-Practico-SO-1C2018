a6e7807788c4bfa97ad8e37f367151b976045b51
MicaFesta <micaelafesta@hotmail.com>
2018-04-20 17:54:04 -0300

    Agrego el Planificador como servidor

--- Planificador/Main.c
+++ Planificador/Main.c
@@ -2,6 +2,7 @@
 
 
 int main(int argc, char ** argv){
-	consola();
+//	consola();
+	crearServidor2();
 	return EXIT_SUCCESS;
 }
--- Planificador/Planificador.h
+++ Planificador/Planificador.h
@@ -1,8 +1,28 @@
-#include <stdlib.h>
+#ifndef CLIENTE_H_
+#define CLIENTE_H_
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
-#include <readline/readline.h>
+#include <stdlib.h> // Para malloc
+#include <netdb.h> // Para getaddrinfo
+#include <unistd.h> // Para close
+#include <signal.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <openssl/md5.h> // Para calcular el MD5
+#include <sys/socket.h> // Para crear sockets, enviar, recibir, etc
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <readline/readline.h> // Para usar readline
 #include <readline/history.h>
+#include <commons/log.h>
+#include <commons/collections/list.h>
+#define IP "127.0.0.1"
+#define PUERTO 8080
 
 void consola();
+
+int crearServidor2(void);
+void sigchld_handler(int s);
+
+#endif /* CLIENTE_H_ */
