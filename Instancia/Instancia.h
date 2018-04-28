ea9386f651a0f10b69808436b414244390c1638e
Julian <schifferjulian@gmail.com>
2018-04-28 18:24:20 -0300

    Limpio los includes del resto de los proyectos

--- Instancia/Instancia.h
+++ Instancia/Instancia.h
@@ -1,16 +1,8 @@
 #ifndef INSTANCIA_H_
 #define INSTANCIA_H_
-#include <stdio.h>
 #include <string.h>
-#include <stdlib.h> // Para malloc
-#include <openssl/md5.h> // Para calcular el MD5
 #include <netdb.h> // Para getaddrinfo
-#include <unistd.h> // Para close
-#include <sys/socket.h> // Para crear sockets, enviar, recibir, etc
-#include <readline/readline.h> // Para usar readline
-#include <commons/log.h>
 #include <commons/config.h>
-#include <commons/collections/list.h>
 #include <Funciones/logs.h>
 
 
--- Planificador/Planificador.h
+++ Planificador/Planificador.h
@@ -1,23 +1,10 @@
 #ifndef PLANIFICADOR_H_
 #define PLANIFICADOR_H_
-#include <stdio.h>
 #include <string.h>
-#include <stdlib.h> // Para malloc
-#include <netdb.h> // Para getaddrinfo
-#include <unistd.h> // Para close
 #include <signal.h>
-#include <errno.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <openssl/md5.h> // Para calcular el MD5
-#include <sys/socket.h> // Para crear sockets, enviar, recibir, etc
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <readline/readline.h> // Para usar readline
-#include <readline/history.h>
-#include <commons/log.h>
 #include <commons/config.h>
-#include <commons/collections/list.h>
 #include <Funciones/logs.h>
 
 
