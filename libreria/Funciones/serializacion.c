1d344e3ae561f879b60dfe58fd11812b5e15f68d
Julian <schifferjulian@gmail.com>
2018-06-07 01:04:56 -0300

    Arreglo una cosita de la libreria

--- libreria/Funciones/serializacion.c
+++ libreria/Funciones/serializacion.c
@@ -287,24 +287,3 @@ int ConectarAServidorESI(int puertoAConectar, char* ipAConectar, proceso servido
 	return socketFD;
 
 }
-
-int ConectarAServidorPlanificador(int puertoAConectar, char* ipAConectar, char servidor[13],
-		char cliente[13], void RecibirHandshake(int socketFD, char emisor[13]), void EnviarHandshake(int socketFD, char emisor[13])) {
-	int socketFD = socket(AF_INET, SOCK_STREAM, 0);
-
-	struct sockaddr_in direccion;
-
-	direccion.sin_family = AF_INET;
-	direccion.sin_port = htons(puertoAConectar);
-	direccion.sin_addr.s_addr = inet_addr(ipAConectar);
-	memset(&(direccion.sin_zero), '\0', 8);
-
-	while (connect(socketFD, (struct sockaddr *) &direccion, sizeof(struct sockaddr))<0)
-		sleep(1); //Espera un segundo y se vuelve a tratar de conectar.
-	EnviarHandshake(socketFD, cliente);
-	RecibirHandshake(socketFD, servidor);
-	return socketFD;
-
-}
-
-
