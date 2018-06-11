23312c1c8bbb2798ad6611f588254107f2ff5196
Zheoden <schifferjulian@gmail.com>
2018-06-11 19:11:51 -0300

    Agrego el make clean al script, para evitar errores de compilaciones anteriores

--- deploy.sh
+++ deploy.sh
@@ -22,18 +22,22 @@ sudo cp liblibreria.so /usr/lib/
 cd 
 
 cd /home/utnso/Proyectos/tp-2018-1c-PC-citos/Coordinador/Debug/
+make clean
 make all
 cd
 
 cd /home/utnso/Proyectos/tp-2018-1c-PC-citos/Instancia/Debug/
+make clean
 make all
 cd
 
 cd /home/utnso/Proyectos/tp-2018-1c-PC-citos/ESI/Debug/
+make clean
 make all
 cd
 
 cd /home/utnso/Proyectos/tp-2018-1c-PC-citos/Planificador/Debug/
+make clean
 make all
 cd
 
