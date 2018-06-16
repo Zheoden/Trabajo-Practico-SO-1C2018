f533f8d64141cc4bd1f77c21473178102a9a66ae
Marcos Ortiz <ortizmarcosn@gmail.com>
2018-06-16 15:07:46 -0300

    Agrego logger en el main con su creación.

--- Coordinador/Main.c
+++ Coordinador/Main.c
@@ -1,6 +1,9 @@
 #include "Coordinador.h"
 
 int main(void) {
+
+	char* coordinador_log_operaciones = strdup("/home/utnso/Proyectos/tp-2018-1c-PC-citos/Coordinador/OperacionesCoordinador.log");
+	crearLogger(coordinador_log_operaciones, "OperacionesCoordinador", 0);
 	char* coordinador_log_ruta = strdup("/home/utnso/Proyectos/tp-2018-1c-PC-citos/Coordinador/Coordinador.log");
 	crearLogger(coordinador_log_ruta, "Coordinador", 0);
 	char* coordinador_config_ruta = strdup("/home/utnso/Proyectos/tp-2018-1c-PC-citos/Coordinador/Coordinador.cfg");
