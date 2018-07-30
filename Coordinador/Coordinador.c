7a4c8df06b6bf8b477fd0b57a1b87ca00a0f9a19
Zheoden <schifferjulian@gmail.com>
2018-07-30 16:05:30 -0300

    Prueba de pirntf

--- Coordinador/Coordinador.c
+++ Coordinador/Coordinador.c
@@ -493,6 +493,7 @@ void coordinarESI(int socket, Paquete paquete, void* datos){
 					EnviarDatosTipo(socketSiguiente, COORDINADOR, sendInstancia, tam, t_SET);
 				} else {
 					//error, no hay instancias conectadas al sistema
+					prinft("No hay Instancias en el sistema. No se puede procesar el pedido.\n");
 				};
 				free(sendInstancia);
 
